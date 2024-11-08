cf72b840513030804d6dadc3ce98a77ba3479ba7
androsgithub <andreemaprinc@gmail.com>
2024-11-07 22:14:40 -0300

    a

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,23 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
-    width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+  width: 100%;
+  height: 42px;
+  background-color: #81259d;
+  color: #fff;
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
-
-    &:hover {
-        opacity: 0.6;
-        cursor:pointer;
-    }
-`
+  border: 1px solid #81259d;
+  border-radius: 21px;
+  &:disabled {
+    background-color: #a38caa;
+    border: 1px solid #86738b;
+  }
+  &:hover:disabled {
+    opacity: 1;
+    cursor: default;
+  }
+  &:hover {
+    opacity: 0.6;
+    cursor: pointer;
+  }
+`;
